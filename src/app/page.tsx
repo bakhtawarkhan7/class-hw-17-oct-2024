ae28be6c3ae7bbb7609d7cdff59090d82277adb6
bakhtawarkhan/7 <bakhtawarashfaq0@gmail.com>
2024-10-27 20:06:56 +0500

    added portfolio description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
       <main className="p-8">
         <section id="about" className="mb-12">
           <h2 className="text-3xl font-bold mb-4">About Us</h2>
-          <p className="text-lg text-gray-700">I am a dedicated front-end developer</p>
+          <p className="text-lg text-gray-700">I'm a dedicated frontend developer with a passion for creating seamless and engaging user interfaces. My expertise lies in frameworks like Next.js and React, where I focus on translating designs into responsive, accessible, and high-performance code. I collaborate closely with designers and backend teams to ensure smooth project delivery. I value attention to detail and continuously update my skills to keep up with the latest in frontend technology</p>
         </section>
         <section id="portfolio" className="mb-12">
           <h2 className="text-3xl font-bold mb-4">My Portfolio</h2>
